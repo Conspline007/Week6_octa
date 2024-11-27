f342c8afdd51a4761b0000696280e2331b87b4ed
Conspline007 <thamaraikamal07@gmail.com>
2024-11-27 05:49:16 +0530

    update

--- strcpy_alt.c
+++ strcpy_alt.c
@@ -2,18 +2,27 @@
 #include <string.h>
 int main()
 {
-    int i=0;
-	char a[4]="Hell";	
-	char b[4]="B";
-	
-	for (i=0;  (a[i]!='\0' && b[i]!= '\0')  ;i++)
-	a[i]=b[i];
-	
-	printf("The output after copying string 2 into string 1 is \n");
-	for (i=0;i<strlen(a);i++)
+    int s1_len,s2_len,i=0;
+	printf("Enter length of 1st string: "); scanf ("%d",&s1_len);
+	printf("Enter length of 2nd string: "); scanf ("%d",&s2_len);
+	char s1[s1_len+1],s2[s2_len+1];
+
+	getchar();
+
+	printf("Enter 1st string: "); fgets(s1,s1_len+1,stdin);
+
+	getchar();
+
+	printf("Enter 2nd string: "); fgets(s2,s2_len+1,stdin);
+
+	while (s1[i]!='\0' && s2[i]!='\0')
 	{
-		printf("%c",a[i]);
+        s1[i]=s2[i];
+		i++;
 	}
+	s1[i]='\0';
+
+	printf("!st string now : %s",s1); 
 
 	return 0;
 }
